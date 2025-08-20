e46639b20776ac05931a5e3738f9f607049ad2ea
Andrea <andrea.giovanardi6@studio.unibo.it>
2025-08-20 16:40:42 +0200

    chore: rename project from my_project to MoneyMate

--- MoneyMate/__main__.py
+++ MoneyMate/__main__.py
@@ -1,7 +1,7 @@
-import my_project
+import MoneyMate
 
 # this is the main module of your app
 # it is only required if your project must be runnable
 # this is the script to be executed whenever some users writes `python -m my_project` on the command line, eg.
-x = my_project.MyClass().my_method()
+x = MoneyMate.MyClass().my_method()
 print(x)
