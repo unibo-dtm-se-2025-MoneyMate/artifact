2212205bcaa13f86980f19622a10ad0bacfbea5a
Cristian Romeo <cristian11romeo@gmail.com>
2025-11-06 16:14:31 +0100

    gui: delate of data_layer in the artifact

--- data_layer/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Compatibility shim: expose DatabaseManager at top-level 'data_layer' for tests.
-
-The implementation lives under MoneyMate.data_layer.manager.
-"""
-from MoneyMate.data_layer.manager import DatabaseManager
-
-__all__ = ["DatabaseManager"]
